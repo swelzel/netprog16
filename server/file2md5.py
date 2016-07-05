8c691b1ecff00dd85350fe7aa5a175e9e6bb09e8
Alexander Kniep <akniep@techfak.uni-bielefeld.de>
2016-07-05 13:50:01 +0200

    beispielimplementierung

--- server/file2md5.py
+++ server/file2md5.py
@@ -8,3 +8,8 @@ def hashfile(afile, hasher=hashlib.md5(), blocksize=65536):
         hasher.update(buffer)
         buffer = afile.read(blocksize)
     return hasher.hexdigest()
+	
+# Beispielimplementierung:
+# _file = open("TinyCore-current.iso","rb")
+# print(hashfile(_file))
+# -> 2298e7b6d1055921aa80983fb31bb933
