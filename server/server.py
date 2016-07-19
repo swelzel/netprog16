0ae20ed44738c45054858182ac9ad47abf4b76d5
Alexander Kniep <alexander.kniep@gmail.com>
2016-07-19 10:22:44 +0200

    syntaxfehehler behoben

--- server/server.py
+++ server/server.py
@@ -2,48 +2,50 @@ from flask import Flask , request
 import hashlib
 app = Flask(__name__)
 
+global clientHashmap
 clientHashmap = dict({})
+global filenames
 filenames = {"kernel-nvidia":"TinyCore-current.iso",
 	"kernel-ati":"TinyCore-current.iso"}
+global localKernelHash
 localKernelHash = "myMD5Checksum"
 
 @app.route("/S2C_Alive", methods=['POST', 'GET'])
 def alive():
 	mac = request.values.get("mac")
 	config2hashmap(request.values.get("cpu"), request.values.get("graka"), mac)
-	global clientHashmap
-	if clientHashmap[mac] == null:
-		clientHashmap[mac] = config
+	#if clientHashmap[mac] == null:
+	#	clientHashmap[mac] = config
 	return "the cake is a lie"
 
 def config2hashmap(cpu, graka, mac):
 	if(cpu == "x86"):
 		if(graka == "nvidia"):
-			global clientHashmap[mac] = "kernel-nvidia"
+			clientHashmap[mac] = "kernel-nvidia"
 		elif(graka == "ati"):
-			global clientHashmap[mac] = "kernel-ati"
+			clientHashmap[mac] = "kernel-ati"
 	elif(cpu == "x64"):
 		if(graka == "nvidia"):
-			global clientHashmap[mac] = "kernel-nvidia"
+			clientHashmap[mac] = "kernel-nvidia"
 		elif(graka == "ati"):
-			global clientHashmap[mac] = "kernel-ati"
+			clientHashmap[mac] = "kernel-ati"
 		
 #die clientseite will selber vergleichen 
 @app.route("/S2C_AnswerKernel", methods=['POST', 'GET'])
 def av_Kernel():
 	mac = request.values.get("mac")
-	maHash = request.values.get("hash")
+	maHash = request.valueglobals.get("hash")
 	
-	if(maHash == "" || maHash.lenght != 32):
-		return false
+	if(maHash == "" or maHash.lenght != 32):
+		return "False"
 	else:
-		_file = open(global filenames["kernel-nvidia"],"rb")
+		_file = open(filenames["kernel-nvidia"],"rb")
 		lacalKernelHash = hashfile(_file)
 		if(maHash != localKernelHash):
-			return true
+			return "True"
 		else:
-			return false
-	return true
+			return "False"
+	return "True"
 
 @app.route("/S2C_SendKernel", methods=['POST', 'GET'])
 def kernel():
@@ -52,7 +54,7 @@ def kernel():
 
 @app.route("/S2C_AnswerCore", methods=['POST', 'GET'])
 def av_Core():
-	ans = true
+	ans = "True"
 	return ans
 
 @app.route("/S2C_SendCore", methods=['POST', 'GET'])
@@ -61,7 +63,7 @@ def core():
 
 @app.route("/S2C_AnswerPackage", methods=['POST', 'GET'])
 def av_Package():
-	return true
+	return "True"
 
 @app.route("/S2C_SendPackage", methods=['POST', 'GET'])
 def package():
